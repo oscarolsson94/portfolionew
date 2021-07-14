4ba464c1c814590a93d30b378bd470a4e5c0586f
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-14 11:32:53 +0200

    creating a custom span component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,7 @@ import {
   Div3,
   NavLink,
   SocialIcons,
+  Span,
 } from "./HeaderStyles";
 
 const Header = () => (
@@ -17,7 +18,7 @@ const Header = () => (
     <Div1>
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
