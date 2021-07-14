cb0e3e1adcca7b65de7b5c2bb35848a79fe4aa4c
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-14 12:26:21 +0200

    adding buttons to code and website source

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -42,6 +42,10 @@ const Projects = () => (
                 ))}
               </TagList>
             </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>Code</ExternalLinks>
+              <ExternalLinks href={visit}>Source</ExternalLinks>
+            </UtilityList>
           </BlogCard>
         )
       )}
