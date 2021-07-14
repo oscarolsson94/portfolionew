056e77de83049edb2623fb4d57332a7e687280f4
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-14 11:33:41 +0200

    aligning portfolio button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,14 @@ const Header = () => (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "20px",
+          }}
+        >
           <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
         </a>
       </Link>
