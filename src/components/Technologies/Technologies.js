fbddcc55f3b6a078a27d32b9b2828f82d7e3e049
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-07-14 12:47:27 +0200

    Getting icons and completing technologoes section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -23,12 +23,32 @@ const Technologies = () => (
     </SectionText>
     <List>
       <ListItem>
-        <DiFirebase size="3rem" />
+        <DiReact size="3rem" />
         <ListContainer>
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
             Experience with: <br />
             React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with: <br />
+            Node.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with: <br />
+            Figma
           </ListParagraph>
         </ListContainer>
       </ListItem>
